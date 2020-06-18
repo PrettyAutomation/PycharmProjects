ec03ec17c44f5a42a8457ac9b77e1112f13b3e99
pretty.sanwale <pretty.sanwale@servicenow.com>
2020-06-18 12:54:30 +0530

    Test:Added Pytest Project

---
+++ PytestProject/SeleniumWithPytest/TestCases/sample.md
@@ -0,0 +1,15 @@
+##### Naming convention for pytest
+test_*.py
+
+*_test.py
+
+##### How to run the test cases in pytest
+1) Naming of functions should be similar to test case file name
+2) To run the test case open terminal
+   go inside the directory write down
+   
+   pytest (testfile name) -> enter
+3) Make sure it shows collected (no. of functions present in file) items is populated   
+
+##### 
+
---
+++ PytestProject/SeleniumWithPytest/TestCases/sample.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome('/Users/pretty.sanwale/PytestProject/SeleniumWithPytest/driverfile/chromedriver')
+driver.maximize_window()
+driver.implicitly_wait(10)
+driver.get('https://opensource-demo.orangehrmlive.com/')
+driver.find_element_by_id('txtUsername').send_keys("Admin")
+driver.find_element_by_id('txtPassword').send_keys('admin123')
+driver.find_element_by_id('btnLogin').click()
+driver.close()
+driver.quit()
+print('Test Passed')
